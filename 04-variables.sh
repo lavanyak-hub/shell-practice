4dde516ad58e85e68ff7254c664fb230c6d76602
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-09-25 21:20:40 +0530

    some message

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-PERSON1=
-PERSON2= 
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1:: Welcome to Alaska $PERSON2"
 echo "$PERSON2:: Thank you $PERSON1, Finally not on Zoom Wedinar"
 echo "$PERSON1:: so, do we have a deal"
-echo "$PERSON2:: Yes, if you make Ukaraine joins Russia"
+echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
