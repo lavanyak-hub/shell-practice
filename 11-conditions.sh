67b3bb1f31cd20c48c2adedd3fba67063f15c2cc
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-12 16:28:33 +0530

    variables

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -8,5 +8,5 @@ if [ $(($NUMBER % 2)) -eq 0 ]; then
 else
    echo "Given number $NUMBER is ODD"
    fi
+ 
 
-   
---
+++ 12-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter the number"
+read NUMBER
+
+if [ $NUMBER -eq 1 ]; then
+   echo "Give number $NUMBER is prime"
+else
+echo "Given number $NUMBER is not a prime"
+
+if
