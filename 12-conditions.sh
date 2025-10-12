b226ca4eb73555475aaf9317776cf8280d0ff546
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-12 16:38:58 +0530

    variables

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -3,7 +3,7 @@
 echo "Please enter the number"
 read NUMBER
 
-if [ "$is_prime" -eq 1 ]; then
+if [ $(($is_prime)) -eq 1 ]; then
   echo "$NUMBER is a prime number."
 else
   echo "$NUMBER is not a prime number."
