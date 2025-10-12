51b1da38c81fd13c57178786533f5d16af46118e
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-12 15:20:41 +0530

    variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -8,7 +8,9 @@ NAME=DevOps
 SUM=$(($NUMBER1+$NUMBER2+$NAME))
 
 echo "SUM is: ${SUM}"
-
+ 
+ # Size = 4, Max index = 3
+ 
 LEADERS=("Modi" "Putin" "Trudo" "Trump")
 
 echo "All leaders: ${LEADERS[@]}"
