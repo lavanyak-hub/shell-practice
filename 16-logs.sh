5bcf2afcde387aa8db097022a67bfe8ceaacd368
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-13 17:07:31 +0530

    some message

--- 16-logs.sh
+++ 16-logs.sh
@@ -6,11 +6,19 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)"
+
 if [ $USERID -ne 0 ]; then
    echo "ERROR:: Please run this script with root privelege"
    exit 1 # failure is other then 0
 
    fi
+
 VALIDATE(){
 
    if [ $1 -ne 0 ]; then
@@ -22,36 +30,34 @@ else
     fi
 }
 
-dnf list installed mysql
+dnf list installed mysql &>>$LOG_FILE
 # Install if it is not found
 
 if [ $? -ne 0 ]; then
 
-dnf install nginx -y
+dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "MySQL"
 else
 echo -e "MySQL already exist ... $Y SKIPPING $N"
 
 fi
 
-dnf list installed nginx
+dnf list installed nginx &>>$LOG_FILE
 if [ $? -ne 0 ]; then
 
-dnf install nginx -y
+dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Nginx"
 else
 echo -e "Nginx already exist ... $Y SKIPPING $N"
 
 fi
 
-dnf list installed python3
+dnf list installed python3 &>>$LOG_FILE
 if [ $? -ne 0 ]; then
 
-dnf install python3 -y
+dnf install python3 -y &>>$LOG_FILE
 VALIDATE $? "python3"
 else
 echo -e "python3 already exist ... $Y SKIPPING $N"
 
 fi 
-
-
