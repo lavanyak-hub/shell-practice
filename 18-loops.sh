c8c55baf3f18a01f00c2a51a97d07f367a4c4552
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-13 19:30:42 +0530

    some message

---
+++ 18-loops.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+   echo "ERROR:: Please run this script with root privelege"
+   exit 1 # failure is other then 0
+
+   fi
+
+VALIDATE(){
+
+   if [ $1 -ne 0 ]; then
+     echo -e "INSTALLING $2 ... $R is failure $N" | tee -a $LOG_FILE
+     exit 1
+else
+    echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+
+    fi
+}
+
+for package in $@
+do
+  # check package is already installed or not 
+  echo "package is: $package"
+done
+  
