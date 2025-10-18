9691f823c61568d277fed5a2e8f1a2fc161e1d9a
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-18 12:59:33 +0530

    some message

--- 19-set.sh
+++ 19-set.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-set -e 
+set -e
+
+error(){
+      echo "Tere is an error"
+
+}
+trap error ERR
 
 echo "Hello.."
 echo "Before error.."
-gggyayd
+gggyayd;dnf # here shell understands there is an error and signal is ERR
 echo "After error"
