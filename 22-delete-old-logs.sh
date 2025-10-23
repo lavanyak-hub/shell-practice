98326982e6fabfcd6cd8141d7a81d68ed69af185
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-23 17:36:56 +0530

    some message

--- 22-delete-old-logs.sh
+++ 22-delete-old-logs.sh
@@ -19,5 +19,11 @@ if [ ! -d $SOURCE_DIR ]; then
 echo -e "ERROR:: $SOURCR_DIR does not exist"
 exit 1
 fi 
-FILES_TO_SELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
 
+while IFS= read -r filepath
+
+do
+echo "Deleting the file: $filepath"
+
+done <<< $FILES_TO_DELETE
