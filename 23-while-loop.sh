fe6247a36816c1d2101467642867e5ee478f3c28
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-23 17:31:31 +0530

    some message

--- 23-while-loop.sh
+++ 23-while-loop.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 
-count=5
+# count=5
 
 
-echo "Starting countdown..."
+# echo "Starting countdown..."
 
-while [ $count -gt 0 ]
-do 
-echo "Time left: $count"
-sleep 1 # pause for 1 second
-count=$((count - 1)) # Decrement the count
-done
-echo "Times up!"
+# while [ $count -gt 0 ]
+# do 
+# echo "Time left: $count"
+# sleep 1 # pause for 1 second
+# count=$((count - 1)) # Decrement the count
+# done
+# echo "Times up!"
+
+
+while IFS= read -r line; do
+
+echo "Processing line: $line"
+
+done < 20-script-1.sh
