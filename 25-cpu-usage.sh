49d6e4cba860de601238f8653c0e6ef5ca76c34b
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-25 16:46:26 +0530

    some message

--- 25-cpu-usage.sh
+++ 25-cpu-usage.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 # in projects we keep it as 75
 
 while IFS= read -r line
 do 
-echo "Line: $line"
-
+USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+PARTISION=(echo $line | awk '{print $7}')
+if [ $USAGE -ge $DISK_THRESHOLD ]; then
+echo "High Usage on $PARTITION: $USAGE"
+fi
 done <<< $DISK_USAGE
